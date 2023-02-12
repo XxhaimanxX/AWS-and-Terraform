8e7889238623d503a3e05031302f5ab68cc98d33
XxhaimanxX <israelhaiman@gmail.com>
2023-02-12 20:57:58 +0200

    fixing user data

--- Homework4/servers/variables.tf
+++ Homework4/servers/variables.tf
@@ -7,9 +7,6 @@ variable "image_id" {
 variable "web_instance_type" {
   type = string
 }
-variable "user_data" {
-  type = string
-}
 variable "key_pair" {
   type = string
 }
@@ -18,4 +15,19 @@ variable "db_instance_type" {
 }
 variable "s3_logs_bucket" {
   type = string
+}
+variable "user_data" {
+  type = string
+  default = <<EOF
+  #! /bin/bash
+  sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+  sudo yum update -y
+  sudo yum install -y epel-release
+  sudo yum install nginx -y
+  sudo systemctl start nginx
+  sudo systemctl enable nginx
+  echo "<h1>Welcome to Grandpa's Whiskey ($(hostname -f))</h1>" | sudo tee /usr/share/nginx/html/index.html > /dev/null
+  echo "* * * * * /usr/bin/aws s3 cp /var/log/nginx/access.log s3://grandpa-whiskey-logs/access.log" | sudo tee mycron
+  crontab mycron
+  EOF
 }
